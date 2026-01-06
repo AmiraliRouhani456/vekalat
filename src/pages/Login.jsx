0c514c7dabf933d4f938e5ecbbf417f37a38d0ef
Ali-Asadian-456 <142652407+Ali-Asadian-456@users.noreply.github.com>
2026-01-07 03:19:08 +0330

    login page correction

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -37,7 +37,8 @@ export default function Login() {
       localStorage.setItem("isLoggedIn", true);
       navigate(`/dashboard/${username}`);
     } else {
-      alert("نام کاربری یا رمز عبور اشتباه است");
+      alert("مشخصات کاربر اشتباه است");
+      generateCaptcha();
     }
   };
 
@@ -77,137 +78,169 @@ export default function Login() {
         </div>
 
         <div dir="rtl" className="bg-white px-8 text-sm ">
-          
-            <div className="flex rounded-md overflow-clip">
-              <div className="bg-yellow-50 py-2 px-2">
-                <FiAlertTriangle className="text-4xl text-red-600" />
-              </div>
-              <ul className="bg-gray-200 block w-full  py-4 px-4 space-y-2 text-xs">
-                <li className="flex gap-1 items-center">
-                  <PiDotsNineBold />
-                  <span>
-                    در هنگام وارد کردن اطلاعات، زبان کيبورد سيستم خود را
-                    تغييرندهيد.
-                  </span>
-                </li>
-                <li className="flex gap-1 items-center">
-                  <PiDotsNineBold />
-                  <span>كیبورد سيستم بر روي En باشد</span>
-                </li>
-                <li className="flex gap-1 items-center">
-                  <PiDotsNineBold />
-                  <span>کليد Caps lock بايد خاموش باشد.</span>
-                </li>
-                <li className="flex gap-1 items-center">
-                  <PiDotsNineBold />
-                  <span>
-                    نتایج آزمون کارآموزی وکالت براساس قانون تسهیل صدور مجوز های
-                    کسب و کار بدون ظرفیت و با لحاظ تراز نمرات اعلام شده است.
-                  </span>
-                </li>
-                <li className="flex gap-1 items-center">
-                  <PiDotsNineBold />
-                  <span>
-                    در صورت هرگونه سوال در رابطه با نمره آزمون و یا سهمیه شرکت
-                    در آزمون، می توانید موضوع خود را فقط در قسمت سیستم پاسخگویی
-                    این سازمان حداکثر تا تاریخ 1404/09/29 مطرح نمایند.
-                  </span>
-                </li>
-                <li className="flex gap-1 items-center">
-                  <PiDotsNineBold />
-                  <span>
-                    داوطلبان دارای حد نصاب ، به اطلاعیه های کانون متبوع خود و
-                    مهلت های اعلام شد جهت ثبت نام در سایت اختصاصی آن کانون توجه
-                    فرمایند.
-                  </span>
-                </li>
-              </ul>
+          <div className="flex rounded-md overflow-clip">
+            <div className="bg-yellow-50 py-2 px-2">
+              <FiAlertTriangle className="text-4xl text-red-600" />
             </div>
-          
+            <ul className="bg-gray-200 block w-full  py-4 px-4 space-y-2 text-xs">
+              <li className="flex gap-1 items-center">
+                <PiDotsNineBold />
+                <span>
+                  در هنگام وارد کردن اطلاعات، زبان کيبورد سيستم خود را
+                  تغييرندهيد.
+                </span>
+              </li>
+              <li className="flex gap-1 items-center">
+                <PiDotsNineBold />
+                <span>كیبورد سيستم بر روي En باشد</span>
+              </li>
+              <li className="flex gap-1 items-center">
+                <PiDotsNineBold />
+                <span>کليد Caps lock بايد خاموش باشد.</span>
+              </li>
+              <li className="flex gap-1 items-center">
+                <PiDotsNineBold />
+                <span>
+                  نتایج آزمون کارآموزی وکالت براساس قانون تسهیل صدور مجوز های
+                  کسب و کار بدون ظرفیت و با لحاظ تراز نمرات اعلام شده است.
+                </span>
+              </li>
+              <li className="flex gap-1 items-center">
+                <PiDotsNineBold />
+                <span>
+                  در صورت هرگونه سوال در رابطه با نمره آزمون و یا سهمیه شرکت در
+                  آزمون، می توانید موضوع خود را فقط در قسمت سیستم پاسخگویی این
+                  سازمان حداکثر تا تاریخ 1404/09/29 مطرح نمایند.
+                </span>
+              </li>
+              <li className="flex gap-1 items-center">
+                <PiDotsNineBold />
+                <span>
+                  داوطلبان دارای حد نصاب ، به اطلاعیه های کانون متبوع خود و مهلت
+                  های اعلام شد جهت ثبت نام در سایت اختصاصی آن کانون توجه
+                  فرمایند.
+                </span>
+              </li>
+            </ul>
+          </div>
         </div>
 
-        <div className="flex">
+        <div className="flex w-full bg-white justify-center px-24 gap-16">
           <form
             onSubmit={handleLogin}
-            className="w-full  bg-white p-6 space-y-6 shadow  min-h-screen"
+            className=" w-full bg-white  pt-8 min-h-screen "
           >
-            <input
-              className="w-full border p-2 rounded"
-              placeholder="Username"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-
-            <input
-              type="password"
-              className="w-full border p-2 rounded"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-
-            {/* CAPTCHA */}
-            <div className="flex items-center justify-between">
-              <div className="bg-gray-200  rounded font-mono text-lg tracking-widest">
-                <button
-                  type="button"
-                  onClick={generateCaptcha}
-                  className="text-2xl text-gray-800 font-semibold px-6 py-3"
-                >
-                  {captcha}
-                </button>
+            <div className=" space-y-6 border p-4 ">
+              <h2 className="text-center bg-gray-200 p-4">
+                جستجوی نتیجه با اطلاعات پرونده
+              </h2>
+              <div className="flex flex-row-reverse gap-2 items-center justify-end">
+                <label className="text-nowrap"> شماره پرونده</label>
+                <input
+                  className="w-[190px] border p-1 rounded"
+                  placeholder=" "
+                  onChange={(e) => setUsername(e.target.value)}
+                />
               </div>
+
+              <div className="flex flex-row-reverse gap-2 items-center justify-end">
+                <label className="text-nowrap"> شماره شناسنامه</label>
+                <input
+                  type="password"
+                  className="w-[190px] border p-1 rounded"
+                  placeholder=""
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+
+              {/* CAPTCHA */}
+              <div className="flex flex-row-reverse items-center">
+                <span dir="rtl" className="text-sm  ">
+                  عبارت مقابل را عينا در کادر عبارت امنیتی درج كنيد :
+                </span>
+                <div className="flex items-center justify-between">
+                  <div className="bg-gray-200  rounded font-mono text-lg tracking-widest">
+                    <button
+                      type="button"
+                      onClick={generateCaptcha}
+                      className="text-2xl text-gray-800 font-semibold px-6 py-3"
+                    >
+                      {captcha}
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <input
+                className="w-full border p-2 rounded"
+                dir="rtl"
+                placeholder="عبارت امنیتی"
+                // value={captchaInput}
+                onChange={(e) => setCaptchaInput(e.target.value)}
+              />
+
+              <button className="w-full bg-gradient-to-b border-pu-500 border from-pink-200 to-pink-500 text-black py-2 rounded">
+                مشاهده نتیجه
+              </button>
             </div>
-
-            <input
-              className="w-full border p-2 rounded"
-              placeholder="Enter the code above"
-              value={captchaInput}
-              onChange={(e) => setCaptchaInput(e.target.value)}
-            />
-
-            <button className="w-full bg-gradient-to-b border-pu-500 border from-pink-200 to-pink-500 text-black py-2 rounded">
-              مشاهده نتیجه
-            </button>
           </form>
           <form
             onSubmit={handleLogin}
-            className="w-full  bg-white p-6 space-y-6 shadow  min-h-screen"
+            className=" w-full   bg-white  pt-8 min-h-screen "
           >
-            <input
-              className="w-full border p-2 rounded"
-              placeholder="Username"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-
-            <input
-              type="password"
-              className="w-full border p-2 rounded"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-
-            {/* CAPTCHA */}
-            <div className="flex items-center justify-between">
-              <div className="bg-gray-200  rounded font-mono text-lg tracking-widest">
-                <button
-                  type="button"
-                  onClick={generateCaptcha}
-                  className="text-2xl text-gray-800 font-semibold px-6 py-3"
-                >
-                  {captcha}
-                </button>
+            <div className=" space-y-6 border p-4 ">
+              <h2 className="text-center bg-gray-200 p-4">
+                جستجوی نتیجه با اطلاعات پرونده
+              </h2>
+              <div className="flex flex-row-reverse gap-2 items-center justify-end">
+                <label className="text-nowrap"> شماره پرونده</label>
+                <input
+                  className="w-[190px] border p-1 rounded"
+                  placeholder=" "
+                  onChange={(e) => setUsername(e.target.value)}
+                />
               </div>
+
+              <div className="flex flex-row-reverse gap-2 items-center justify-end">
+                <label className="text-nowrap"> شماره شناسنامه</label>
+                <input
+                  type="password"
+                  className="w-[190px] border p-1 rounded"
+                  placeholder=""
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+
+              {/* CAPTCHA */}
+              <div className="flex flex-row-reverse items-center">
+                <span dir="rtl" className="text-sm  ">
+                  عبارت مقابل را عينا در کادر عبارت امنیتی درج كنيد :
+                </span>
+                <div className="flex items-center justify-between">
+                  <div className="bg-gray-200  rounded font-mono text-lg tracking-widest">
+                    <button
+                      type="button"
+                      onClick={generateCaptcha}
+                      className="text-2xl text-gray-800 font-semibold px-6 py-3"
+                    >
+                      {captcha}
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <input
+                className="w-full border p-2 rounded"
+                dir="rtl"
+                placeholder="عبارت امنیتی"
+                // value={captchaInput}
+                onChange={(e) => setCaptchaInput(e.target.value)}
+              />
+
+              <button className="w-full bg-gradient-to-b border-pu-500 border from-pink-200 to-pink-500 text-black py-2 rounded">
+                مشاهده نتیجه
+              </button>
             </div>
-
-            <input
-              className="w-full border p-2 rounded"
-              placeholder="Enter the code above"
-              value={captchaInput}
-              onChange={(e) => setCaptchaInput(e.target.value)}
-            />
-
-            <button className="w-full bg-gradient-to-b border-pu-500 border from-pink-200 to-pink-500 text-black py-2 rounded">
-              مشاهده نتیجه
-            </button>
           </form>
         </div>
       </div>
